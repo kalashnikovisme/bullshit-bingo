890a4f253105eccec520e04fa07b4de0596c719c
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2019-05-09 05:00:54 +0400

    remove data.csv

--- export_to_xlsx.rb
+++ export_to_xlsx.rb
@@ -1,11 +1,29 @@
+require 'axlsx'
+
 array = File.readlines('data.csv')
 count = ARGV[0].to_i
 column_count = ARGV[1].to_i
 row_count = ARGV[2].to_i
-count.times do |iteration|
-  ids = (0..array.count - 1).to_a.sort do
-    rand() - 0.5
-  end[0..column_count * row_count]
-  puts ids.join(',')
+Axlsx::Package.new do |p|
+  count.times do |iteration|
+    ids = (0..array.count - 1).to_a.sort do
+      rand() - 0.5
+    end[0..column_count * row_count]
+    p.workbook.add_worksheet name: iteration.to_s do |sheet|
+      row_count.times do |row_index|
+        row = ((0..column_count).to_a.map do |column_index|
+          array[ids[row_index * column_count + column_index]]
+        end)
+        sheet.add_row row,
+          style: sheet.styles.add_style(alignment: {
+            horizontal: :center,
+            vertical: :center,
+            wrap_text: true
+          }),
+          widths: (0..column_count).to_a.map { 25 },
+          height: 100
+      end
+    end
+    p.serialize 'result.xlsx'
+  end
 end
-
